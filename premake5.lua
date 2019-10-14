39382c5b7086a43cffa9629600788270ceb64275
Luiz Carlos Rumbelsperger Viana <maxd13@maxd13-devuan>
2019-10-14 19:33:18 -0400

    Added premake5 build automation to the project. Premake generated makefiles that should be commited. We are gitignoring build and obj folders.

---
+++ premake5.lua
@@ -0,0 +1,48 @@
+workspace "Process Scheduler"
+  configurations {"Debug", "Release"}
+  -- Use C as the target language for all builds
+  language "C"
+  -- cdialect "C11"
+  -- location "build/projects"
+  targetdir "build/bin/%{cfg.buildcfg}"
+
+
+  configuration { "linux", "gmake" }
+
+  filter "configurations:Debug"
+    -- Add the preprocessor definition DEBUG to debug builds
+    defines { "DEBUG" }
+    -- Ensure symbols are bundled with debug builds
+    symbols "On"
+    buildoptions { "-Wall" }
+
+  filter "configurations:Release"
+    -- Add the preprocessor definition NDEBUG to release builds
+    -- This should also disable assertions.
+    defines { "NDEBUG" }
+    -- Turn on compiler optimizations for release builds
+    optimize "On"
+
+  -- -- Scheduler build
+  -- project "Scheduler"
+  --   kind "ConsoleApp"
+  --   -- recursively globs all .h and .c files from src folder.
+  --   files { "src/**.h", "src/**.c" }
+  --   removefiles { "src/interpreter.c" } 
+  
+  -- project "Interpreter"
+    
+  project "ProcessTableTest"
+    kind "ConsoleApp"
+    defines { "TEST" }
+    files { "src/**.h", "src/**.c" }
+    files { "test/process_table_test.c" }
+    files { "test/unity/*.h", "test/unity/*.c" }
+    removefiles { "src/interpreter.c", "src/scheduler.c" } 
+    links { "m" }
+
+  -- project "SchedulerTest"
+  --   kind "ConsoleApp"
+  --   defines { "TEST" }
+  --   links { "Scheduler" }
+  --   files {"test/**.h", "test/**.cpp", "test/pub/*.h"}
